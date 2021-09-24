0199828a41d0cbc95e4e04a0c38344bf97a22149
iamrivera <a3m3rivera@gmail.com>
2021-09-24 02:28:48 -0700

    Successfully added stylized navbar

--- src/components/routes/Navbar.js
+++ src/components/routes/Navbar.js
@@ -1,44 +1,53 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
 
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        <NavLink
-          to="/"
-          exact
-          style={link}
-          activeStyle={{ background: "darkblue" }}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          exact
-          style={link}
-          activeStyle={{ background: "darkblue" }}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/my-pockets"
-          exact
-          style={link}
-          activeStyle={{ background: "darkblue" }}
-        >
-          My Pockets
-        </NavLink>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" >
+            Back of the Napkin ✏
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            wfd-invisible="true"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/">Home</NavLink>
+                  <span className="visually-hidden">(current)</span>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/my-pockets">
+                  My Pockets
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="my-napkins">
+                  My Napkins
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
