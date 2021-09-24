9148681e0feb3c2954845cca2d3e06aa22f30e4e
iamrivera <a3m3rivera@gmail.com>
2021-09-24 02:38:52 -0700

    Added additional wording to Home page.

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -4,10 +4,18 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+        <h3>
+         Welcome to Back of the Napkin!
+        </h3>
+        <p class="lead">
+          A place to store your thoughts from around the web.
+        </p>
+
         <figure class="text-center">
           <blockquote class="blockquote">
             <p class="mb-">
-            I put things down on sheets of paper and stuff them in my pockets. When I have enough, I have a book.
+              I put things down on sheets of paper and stuff them in my pockets.
+              When I have enough, I have a book.
             </p>
           </blockquote>
           <figcaption class="blockquote-footer">
