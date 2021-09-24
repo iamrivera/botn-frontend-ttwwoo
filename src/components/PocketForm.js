91e03878e1b124994fba1b52c18ec82412ebf7d6
iamrivera <a3m3rivera@gmail.com>
2021-09-24 00:08:42 -0700

    Added a stylized form input. Setting up handleChange method.

--- src/components/PocketForm.js
+++ src/components/PocketForm.js
@@ -2,19 +2,40 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class PocketForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pocket: "",
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            pocket: '',
-        }
-    }
+  handleChange(event) {
+    this.setState({
+      pocket: event.target.value,
+    });
+  }
   render() {
     return (
-      <div>
+      <div style={{ maxWidth: "20rem" }}>
         <form>
-          <input type="text" placeholder="Stitch New Pocket" />
-          <input type="submit" />
+          <fieldset>
+            <legend>Stitch a New Pocket</legend>
+            <div className="form-group">
+              <label htmlFor="pocketName" className="form-label mt-4">
+                Pocket Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="pocketName"
+                placeholder="Enter Pocket's Name"
+                onChange={(event) => this.handleChange(event)}
+              ></input>
+            </div>
+          </fieldset>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     );
