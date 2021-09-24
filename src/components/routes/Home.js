99a4ef31a977418d421bec2bee0316e270baa5d9
iamrivera <a3m3rivera@gmail.com>
2021-09-24 02:35:09 -0700

    Added catchy home page quote.

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home!</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <figure class="text-center">
+          <blockquote class="blockquote">
+            <p class="mb-">
+            I put things down on sheets of paper and stuff them in my pockets. When I have enough, I have a book.
+            </p>
+          </blockquote>
+          <figcaption class="blockquote-footer">
+            John Lennon <cite title="Source Title">A Point In Time</cite>
+          </figcaption>
+        </figure>
+      </div>
+    );
+  }
 }
