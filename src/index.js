daa9c76ba4bfb18f20fb387728bdfe12461d21ef
iamrivera <a3m3rivera@gmail.com>
2021-09-23 19:03:48 -0700

    Imported applyMiddleware and thunk. Passed to store const.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import pocketsReducer from "./reducers/pocketsReducer.js";
 
-const store = createStore(pocketsReducer); 
+const store = createStore(pocketsReducer, applyMiddleware(thunk)); 
 
 import './index.css';
 import './bootstrap1.min.css';
