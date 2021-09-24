db78fd22d36d447266686bf113f406dbfe167166
iamrivera <a3m3rivera@gmail.com>
2021-09-23 23:36:09 -0700

    Building Pocket Form that creates new pocket obects.

--- src/components/Pocket.js
+++ src/components/Pocket.js
@@ -3,14 +3,19 @@ import "../bootstrap1.min.css";
 
 const Pocket = (props) => {
   return (
-    <div className="card text-white bg-success mb-20" style={{maxWidth: "20rem"}}> 
-        <div className="class-header">Pocket {props.item.id}</div>
-        <div className="card-body">
-          <h4 className="card-title">{props.item.name}</h4>
-          <p className="card-text">{props.item.description}</p>
-        </div>
+    <div
+      className="card border-primary mb-3"
+      style={{ maxWidth: "20rem" }}
+    >
+      <div className="class-header"></div>
+      <div className="card-body">
+        <h4 className="card-title">My {props.item.name} Pocket</h4>
+        <p className="card-text">{props.item.description}</p>
+      </div>
+      <button type="button" class="btn btn-success">Open</button>
+      <button type="button" class="btn btn-warning">Edit</button>
     </div>
-  )
+  );
 };
 
 export default Pocket;
